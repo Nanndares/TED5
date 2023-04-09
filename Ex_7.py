0c18577070c7e672371db0798d3c5ffb203e8782
Nanndares <fernanda.fcsa@hotmail.com>
2023-04-09 16:13:15 -0300

    Exercicios TED5

---
+++ Ex_7.py
@@ -0,0 +1,17 @@
+##7
+
+quantidade = int(input('Quantas pessoas você quer cadastrar? '))
+cadastro = []
+for c in range(quantidade):
+    print('')
+    nome = str(input('Digite seu nome: '))
+    idade = int(input('Informe sua idade: '))
+    email = str(input('Digite seu email: '))
+    cadastro.append({'nome': nome, 'idade': idade, 'email': email})
+
+print("Cadastro:")
+for item in cadastro:
+    print("Nome:", item['nome'])
+    print("Idade:", item['idade'])
+    print("E-mail:", item['email'])
+    print("")
